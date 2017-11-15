e050cca1263ca22476f87ac97f6ab74c4c84cfd1
Nedjim <nedjim.danimon@gmail.com>
2017-11-15 18:27:58 +0100

    Add Header + Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+        <header>
+          <h1>Redux Basket</h1>
+          <Navbar />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <body>
+        </body>
       </div>
     );
   }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav>
+        <li className='active'><a href="#">Shop</a></li>
+        <li><a href="#">About</a></li>     
+        <li><a href="#">Cart</a></li>      
+    </nav>
+);
+
+export default Navbar;
