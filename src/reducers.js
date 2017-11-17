7eb448173f7c5aef696f51fa4b108b2d1855502a
Mike Romain <mchlrmn@me.com>
2017-11-17 18:11:46 +0100

    Fixed undefined statements.

--- src/reducers.js
+++ src/reducers.js
@@ -1,83 +1,82 @@
 import {
-	ADD_PRODUCT,
-	REMOVE_PRODUCT,
-	EMPTY_CART,
-	SET_QUANTITY,
-	INCREMENT,
-	DECREMENT
+    ADD_PRODUCT,
+    REMOVE_PRODUCT,
+    EMPTY_CART,
+    SET_QUANTITY,
+    INCREMENT,
+    DECREMENT
 } from "./constants";
 
 const initialState = {
-	products: [],
-	cart: []
+    products: [],
+    cart: []
 };
 
-export default (state, action) => {
-	switch (action.type) {
-		case ADD_PRODUCT:
-			return {
-				...state,
-				cart: [...state.cart, action.product]
-			};
-		case REMOVE_PRODUCT:
-			return {
-				...state,
-				cart: state.cart.filter(
-					product => product.id !== action.productId
-				)
-			};
-		case EMPTY_CART:
-			return {
-				...state,
-				cart: []
-			};
-		case SET_QUANTITY:
-			switch (action.setQuantityType) {
-				case INCREMENT:
-					return {
-						...state,
-						cart: cart.map(product => {
-							if (product.id === action.productId) {
-								return {
-									...product,
-									quantity: product.quantity + action.quantity
-								};
-							} else {
-								return product;
-							}
-						})
-					};
-				case DECREMENT:
-					return {
-						...state,
-						cart: cart.map(product => {
-							if (product.id === action.productId) {
-								return {
-									...product,
-									quantity: product.quantity - action.quantity
-								};
-							} else {
-								return product;
-							}
-						})
-					};
-					break;
-				default:
-					return {
-						...state,
-						cart: cart.map(product => {
-							if (product.id === action.productId) {
-								return {
-									...product,
-									quantity: action.quantity
-								};
-							} else {
-								return product;
-							}
-						})
-					};
-			}
-		default:
-			return state;
-	}
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_PRODUCT:
+            return {
+                ...state,
+                cart: [...state.cart, action.product]
+            };
+        case REMOVE_PRODUCT:
+            return {
+                ...state,
+                cart: state.cart.filter(
+                    product => product.id !== action.productId
+                )
+            };
+        case EMPTY_CART:
+            return {
+                ...state,
+                cart: []
+            };
+        case SET_QUANTITY:
+            switch (action.setQuantityType) {
+                case INCREMENT:
+                    return {
+                        ...state,
+                        cart: state.cart.map(product => {
+                            if (product.id === action.productId) {
+                                return {
+                                    ...product,
+                                    quantity: product.quantity + action.quantity
+                                };
+                            } else {
+                                return product;
+                            }
+                        })
+                    };
+                case DECREMENT:
+                    return {
+                        ...state,
+                        cart: state.cart.map(product => {
+                            if (product.id === action.productId) {
+                                return {
+                                    ...product,
+                                    quantity: product.quantity - action.quantity
+                                };
+                            } else {
+                                return product;
+                            }
+                        })
+                    };
+                default:
+                    return {
+                        ...state,
+                        cart: state.cart.map(product => {
+                            if (product.id === action.productId) {
+                                return {
+                                    ...product,
+                                    quantity: action.quantity
+                                };
+                            } else {
+                                return product;
+                            }
+                        })
+                    };
+            }
+        default:
+            return state;
+    }
 };
