dfce2cd74cc781811e5a318d78e42658c8bbe037
Nedjim <nedjim.danimon@gmail.com>
2017-11-17 16:59:21 +0100

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -6,14 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <header>
           <h1>Redux Basket</h1>
           <Navbar />
         </header>
-
-        <body>
-        </body>
       </div>
     );
   }
