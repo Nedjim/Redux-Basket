81761aaec42b49f755fdbdbb14096d0be3f9a030
Nedjim <nedjim.danimon@gmail.com>
2017-11-20 14:17:46 +0100

    Add products

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar.jsx';
+import Products from './components/Products.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='App'>
         <header>
           <h1>Redux Basket</h1>
           {Navbar()}
         </header>
+        {Products()}
       </div>
     );
   }
